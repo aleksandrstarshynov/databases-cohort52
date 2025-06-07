95ac3d922e51d3c0598f9e8f7bbba05821a6888a
Oleksandr Starshynov <80248109+aleksandrstarshynov@users.noreply.github.com>
2025-06-07 20:18:02 +0200

    Update joins.js

--- Week2/Assignment/joins.js
+++ Week2/Assignment/joins.js
@@ -7,33 +7,31 @@ const connection = await mysql.createConnection({
 });
 
 try {
-  // Выбираем базу данных
-  await connection.query("USE w2_assignment");
+  await connection.query("research_db.db");
 
-  // 📌 Запрос 1: Авторы и их менторы
+  // Request 1: Authors and their mentors
   const [authors_mentors] = await connection.query(`
     SELECT a.author_name AS Author, m.author_name AS Mentor, a.mentor AS "Mentor ID"
     FROM authors AS a
     LEFT JOIN authors AS m ON a.mentor = m.author_id
   `);
 
-  console.log("\n📘 Authors and their mentors:");
+  console.log("\n Authors and their mentors:");
   console.table(authors_mentors);
 
-  // 📌 Запрос 2: Авторы и их статьи
+  // Query 2: Authors and articles
   const [authors_papers] = await connection.query(`
     SELECT a.*, rp.paper_title
     FROM authors AS a
     LEFT JOIN authorPapers AS ap ON a.author_id = ap.author_id
     LEFT JOIN research_Papers AS rp ON rp.paper_id = ap.paper_id
-    ORDER BY a.author_id
   `);
 
-  console.log("\n📚 Authors and their research papers:");
+  console.log("\n Authors and research papers:");
   console.table(authors_papers);
 
 } catch (error) {
-  console.error("❌ Error:", error.message);
+  console.error("Error:", error.message);
 } finally {
   await connection.end();
 }
