5c453da5d7ccc3e2e6f809a85c2cdb436f0f2a08
Oleksandr Starshynov <80248109+aleksandrstarshynov@users.noreply.github.com>
2025-06-07 20:17:02 +0200

    Update create_research_papers_table.js

--- Week2/Assignment/create_research_papers_table.js
+++ Week2/Assignment/create_research_papers_table.js
@@ -1,18 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('example.db');
+const db = new sqlite3.Database('research_db.db');
 
 db.serialize(() => {
   db.run(`
-    CREATE TABLE IF NOT EXISTS research_Papers (
-      paper_id INTEGER PRIMARY KEY,
-      paper_title TEXT,
-      conference TEXT,
-      publish_date TEXT
+    CREATE TABLE IF NOT EXISTS authors (
+      author_id INTEGER PRIMARY KEY,
+      author_name VARCHAR(50) NOT NULL,
+      university TEXT NOT NULL,
+      date_of_birth DATE NOT NULL,
+      h_index INTEGER,
+      gender TEXT CHECK(gender IN ('Male', 'Female', 'Prefer not to say'))
     );
   `, (err) => {
-    if (err) console.error('Error creating research_Papers:', err.message);
-    else console.log('Table "research_Papers" created successfully.');
+    if (err) console.error('Error creating table:', err.message);
+    else console.log('Table "authors" created successfully.');
   });
 });
 
